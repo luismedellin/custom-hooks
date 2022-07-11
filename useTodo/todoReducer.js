83453bbefd23d154bd48b7a1f1ebed4438e86226
Luis Velandia <v-lvelandia@youngliving.com>
2022-07-11 10:27:22 -0500

    useTodo agregado

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,25 @@
+export const todoReducer = (initialState, action) => {
+    switch (action.type){
+        case '[TODO] Add Todo':{
+            return  [ ...initialState, action.payload ];
+        }
+            
+
+        case '[TODO] Delete Todo':
+            return initialState.filter(i=> i.id !== action.payload);
+
+        case '[TODO] Toggle Todo':
+            return initialState.map(todo=> {
+                if(todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo;
+            })
+
+        default:
+            return initialState;
+    }
+}
